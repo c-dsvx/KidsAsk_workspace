81b9e5a142b01408fc67712108e30895aed1a3bc
c-dsvx <desvauxchloe@gmail.com>
2024-08-19 12:14:55 +0200

    dialogue structure checked

--- KidAsk-main/src/pages/QaPhase.js
+++ KidAsk-main/src/pages/QaPhase.js
@@ -38,9 +38,14 @@ export default function QaPhase() {
   const [stateSecond, setStateSecond] = React.useState({}) /* second checkbox state */
 
   const [keyword, setKeyword] = useState('') /*user-typed keyword1*/
-  const[isEditable, setIsEditable] = useState(true) /*used for freezing input*/
+  const[isEditable1, setIsEditable1] = useState(true) /*used for freezing input 1*/
+
+  const [linkword, setLinkword] = useState('') /*user-typed link word*/
+  const[isEditable2, setIsEditable2] = useState(true) /*used for freezing input 2*/
 
   const [help1, setHelp1] = useState(false) /* help for finding keywords set to false, true on click */
+
+  const [help2, setHelp2] = useState(false) /* help for finding linkwords set to false, true on click */
 
   const { user } = useContext(UserContext)
 
@@ -79,8 +84,13 @@ export default function QaPhase() {
   }
 
   // when button clicked submit keyword
-  const freezeWord = () => {
-    setIsEditable(false) 
+  const freezeWord1 = () => {
+    setIsEditable1(false) 
+  }
+
+  // when button clicked submit linkword
+  const freezeWord2 = () => {
+    setIsEditable2(false) 
   }
 
   // when button clicked help 1 becomes true
@@ -88,6 +98,10 @@ export default function QaPhase() {
     setHelp1(true)
   } 
 
+  // when button clicked help 2 becomes true
+  const showHelp2 = () => {
+     setHelp2(true)
+  } 
 
   // Update the question state when user types
   const handleChangeQuestion = (e) => {
@@ -99,6 +113,11 @@ export default function QaPhase() {
     setKeyword(e.target.value) /*updates user-typed keyword state*/
   }
 
+  // Update the linkword state when user types
+  const handleChangeLinkword = (e) => {
+    setLinkword(e.target.value) /*updates user-typed linkword state*/
+  }
+
   // Submit user input and move to the next question or slide
   const nextQuestion = async () => {
     await addUserInput(user.identifiant, 'qa-phase', `${id}/slides/${topic.slides[slideIndex].text.substring(0, 40).replace(/\//g, '-')}/questions/${questionIndex}`, {
@@ -106,12 +125,18 @@ export default function QaPhase() {
       prompt1: Object.keys(stateFirst).length ? Object.keys(stateFirst)[0]: "no-prompt", // records checked option in list 1
       keyword,
       prompt2: Object.keys(stateSecond).length ? Object.keys(stateSecond)[0]: "no-prompt", // records checked option in list 2
+      linkword, // records associated keyword
       question // records question typed
     })
     setStateFirst({})
     setStateSecond({})
     setQuestion('')
     setKeyword('')
+    setLinkword('')
+    setIsEditable1(true)
+    setIsEditable2(true)
+    setHelp1(false)
+    setHelp2(false)
     if (questionIndex + 1 < 6) {
       setQuestionIndex(questionIndex + 1)
     } else if (slideIndex + 1 < topic.slides.length) {
@@ -410,9 +435,9 @@ export default function QaPhase() {
                       `Tu peux maintenant essayer par toi-même. Essaye de trouver un mot du texte qui te rend curieux. Écris-le ici : `
                     } />
 
-                    <TextField value={keyword} onChange={handleChangeKeyword} id="standard-basic" label="Mets ton mot ici" fullWidth disabled={!isEditable}/>
+                    <TextField value={keyword} onChange={handleChangeKeyword} id="standard-basic" label="Mets ton mot ici" fullWidth disabled={!isEditable1}/>
                     <ContentButtonWrapper>
-                      <Button onClick={freezeWord} variant="contained" disabled={!keyword}>OK</Button>
+                      <Button onClick={freezeWord1} variant="contained" disabled={!keyword}>OK</Button>
                     </ContentButtonWrapper>           
                     {/*input keyword and freezed if submitted*/}
 
@@ -421,7 +446,7 @@ export default function QaPhase() {
                     } />
 
                     <ContentButtonWrapper> {/* if time, change to square button (see pages at end of app)*/}
-                      <Button onClick={showHelp1} variant="contained" disabled={!isEditable}>Je veux de l'aide</Button>
+                      <Button onClick={showHelp1} variant="contained" disabled={!isEditable1}>Je veux de l'aide</Button>
                     </ContentButtonWrapper>
                  </>   
                 }
@@ -462,7 +487,7 @@ export default function QaPhase() {
                 }
 
                 { // Q2 If at least one checkbox checked or wirtten answer, show second list
-                 (showQuestions && questionIndex === 1 && (Object.keys(stateFirst).length > 0 || isEditable === false /*or state button = clicked*/)) &&
+                 (showQuestions && questionIndex === 1 && (Object.keys(stateFirst).length > 0 || isEditable1 === false /*or state button = clicked*/)) &&
                   <>
                     <ChatMessage text={
                     `Est-ce que ce mot te rappelle quelque-chose que tu connais déjà ? Voici à quoi, moi il me fait penser :`
@@ -543,21 +568,218 @@ export default function QaPhase() {
                     </>
 
                 }
+
+                { // Q3-5 keyword
+                showQuestions && (questionIndex >= 2 && questionIndex <= 4) &&
+                <>
+                    <ChatMessage text={
+                      `Tu peux maintenant le faire tout seul ! Essaye de trouver un mot du texte qui te rend curieux. Écris-le ici : `
+                    } />
+
+                    <TextField value={keyword} onChange={handleChangeKeyword} id="standard-basic" label="Mets ton mot ici" fullWidth disabled={!isEditable1}/>
+                    <ContentButtonWrapper>
+                      <Button onClick={freezeWord1} variant="contained" disabled={!keyword}>OK</Button>
+                    </ContentButtonWrapper>           
+                    {/*input keyword and freezed if submitted*/}
+
+                    <ChatMessage text={
+                      `Si tu n'y arrives pas, je peux t'aider !`
+                    } />
+
+                    <ContentButtonWrapper> {/* if time, change to square button (see pages at end of app)*/}
+                      <Button onClick={showHelp1} variant="contained" disabled={!isEditable1}>Je veux de l'aide</Button>
+                    </ContentButtonWrapper>
+                 </>   
+                }
+
+                { // Q3-5 If user asked for help 1
+                  (showQuestions && (questionIndex >= 2 && questionIndex <= 4) && help1 === true)
+                   &&
+                  <>
+                <ChatMessage text={
+                    `Voici les mots que moi j'ai trouvés dans le texte. Coche celui qui te rend curieux.`
+                 } />
+
+                    <Card variant="outlined">
+                      <CardContent>
+                        <FormControl component="fieldset" className={classes.formControl}>
+                          <FormGroup>
+                            {
+                              topic.slides[slideIndex].questions[questionIndex].subtopic.map(op => {
+                                return <FormControlLabel
+                                  control={
+                                    <Checkbox
+                                      color="primary"
+                                      onChange={handleChangeFirst}
+                                      checked={!!stateFirst[op]}
+                                      name={op} />
+                                  }
+                                  label={op}
+                                />
+                              })
+                            }
+                          </FormGroup>
+                        </FormControl>
+                      </CardContent>
+                    </Card>
+
+                    </>
+                    
+                }
+
+                { // Q3-5 If at least one checkbox checked or wirtten answer, show second input
+                 (showQuestions && (questionIndex >= 2 && questionIndex <= 4) && (Object.keys(stateFirst).length > 0 || isEditable1 === false /*or state button = clicked*/)) &&
+                  <>
+                    <ChatMessage text={
+                    `Ce mot te rappelle sûrement quelque-chose que tu connais déjà ! Écris-le ici :`
+                  } />
+
+                    <TextField value={linkword} onChange={handleChangeLinkword} id="standard-basic" label="Mets ton mot ici" fullWidth disabled={!isEditable2}/>
+                    <ContentButtonWrapper>
+                      <Button onClick={freezeWord2} variant="contained" disabled={!linkword}>OK</Button>
+                    </ContentButtonWrapper>           
+                    {/*input keyword and freezed if submitted*/}
+
+                    <ChatMessage text={
+                      `Si tu n'y arrives pas, je peux t'aider !`
+                    } />
+
+                    <ContentButtonWrapper> {/* if time, change to square button (see pages at end of app)*/}
+                      <Button onClick={showHelp2} variant="contained" disabled={!isEditable2}>Je veux de l'aide</Button>
+                    </ContentButtonWrapper>
+                 </>   
+
+                }
+
+                { // Q3-5 If user asked for help 2
+                  (showQuestions && (questionIndex >= 2 && questionIndex <= 4) && help2 === true)
+                   &&
+                  <>
+                <ChatMessage text={
+                    `Voici les choses auxquelles me font penser les mots-clés du texte. Coche celle qui te rend curieux.`
+                 } />
+
+                    <Card variant="outlined">
+                      <CardContent>
+                        <FormControl component="fieldset" className={classes.formControl}>
+                          <FormGroup>
+                            {
+                              topic.slides[slideIndex].questions[questionIndex].subtopic2.map(op => {
+                                return <FormControlLabel
+                                  control={
+                                    <Checkbox
+                                      color="primary"
+                                      onChange={handleChangeSecond}
+                                      checked={!!stateSecond[op]}
+                                      name={op} />
+                                  }
+                                  label={op}
+                                />
+                              })
+                            }
+                          </FormGroup>
+                        </FormControl>
+                      </CardContent>
+                    </Card>
+
+                    </>
+                    
+                }
+
+                { // Q3-5 If at least one answer in each show input zone
+                  (showQuestions && (questionIndex >= 2 && questionIndex <= 4) && (Object.keys(stateSecond).length > 0 || isEditable2 === false /*or state button = clicked*/))
+                   &&
+                  <>
+                    <ChatMessage text={
+                      `Super ! Tu peux maintenant formuler ta question en utilisant ton ou tes mot(s)-clé(s)`
+                    } />
+
+                    <TextField value={question} onChange={handleChangeQuestion} id="standard-basic" label="Mets ta question ici" fullWidth />
+
+                    <ContentButtonWrapper>
+                      <Button onClick={nextQuestion} variant="contained" disabled={!question}>Soumettre</Button>
+                    </ContentButtonWrapper>
+
+                    <ChatMessage text={
+                    `Je te propose de commencer ta question par le(s) mot(s) '${topic.slides[slideIndex].questions[questionIndex].starter}'. Mais tu peux en choisir un autre si tu veux.`
+                    } >
+                    
+                    </ChatMessage>
+
+                      <EaseUp>
+                      {
+                        <Card>
+                          <CardContent>
+                            <SubTopicList>
+                                {
+                                <div>
+                                  <p>
+                                  Pour rappel, voici une liste de mots interrogatifs que tu pourrais utiliser :
+                                </p>
+                                <ul>
+                                  <li>Quel.s/ Quelle.s</li>
+                                  <li>Pourquoi</li>
+                                  <li>Comment</li>
+                                  <li>Combien</li>
+                                  <li>...</li>
+                                    </ul>
+                                    </div>
+                                
+                                }
+                              </SubTopicList>
+                          </CardContent>
+                        </Card>
+                      }
+                      </EaseUp>
+
+                    </>
+
+                }
               
-                { // Show the final question input if the question index is 5
+                { // Show the final keyword input if the question index is 5
                 showQuestions && questionIndex  === 5 &&
                 <>
                 <ChatMessage text={
-                  `Maintenant essaie de formuler ta question tout seul, comme on s'est entrainés ensemble.`
+                  `Maintenant essaie de formuler ta question tout seul, comme on s'est entrainés à faire ensemble.`
                 } />
                 <ChatMessage text={
-                  `Attention, tu ne peux pas répéter une question précédente et la réponse ne doit pas figurer dans le texte.`
+                  `D'abord, trouve un mot-clé du texte qui te rend curieux et écris-le ici : `
                 } />
                         
-                <TextField id="standard-basic" label="Mets ta question ici" fullWidth onChange={handleChangeQuestion} />
+                <TextField id="standard-basic" label="Mets ton mot-clé ici" fullWidth onChange={handleChangeKeyword} />
 
                 <ContentButtonWrapper>
-                  <Button onClick={nextQuestion} variant="contained" disabled={!question}>Soumettre</Button>
+                  <Button onClick={freezeWord1} variant="contained" disabled={!keyword}>OK</Button>
+                </ContentButtonWrapper>
+                </>
+                } 
+
+                { // Show the final linkword input if the question index is 5
+                showQuestions && questionIndex  === 5 && isEditable1 === false &&
+                <>
+                <ChatMessage text={
+                  `Super ! Maintenant écris ce à quoi te fait penser ce mot-clé :`
+                } />
+                        
+                <TextField id="standard-basic" label="Mets ton idée ici" fullWidth onChange={handleChangeLinkword} />
+
+                <ContentButtonWrapper>
+                  <Button onClick={freezeWord2} variant="contained" disabled={!linkword}>OK</Button>
+                </ContentButtonWrapper>
+                </>
+                } 
+
+                { // Show the final question input if the question index is 5
+                showQuestions && questionIndex  === 5 && isEditable2 === false &&
+                <>
+                <ChatMessage text={
+                  `Parfait ! Tu peux maintenant formuler ta question grâce à tes mots-clés :`
+                } />
+                        
+                <TextField id="standard-basic" label="Mets ta question ici" fullWidth onChange={handleChangeLinkword} />
+
+                <ContentButtonWrapper>
+                  <Button onClick={nextQuestion} variant="contained" disabled={!linkword}>Valider</Button>
                 </ContentButtonWrapper>
                 </>
                 } 
